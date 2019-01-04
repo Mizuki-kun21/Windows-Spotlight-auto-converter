7303097a19e35915c5b73511a287c4d2a99d2db9
Bart Jansen <42934991+Mizuki-kun21@users.noreply.github.com>
2019-01-04 17:38:57 +0100

    Version 1.0

---
+++ Windows spotlight.bat
@@ -0,0 +1,5 @@
+Xcopy /H /I /K "%localappdata%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\*.*" "%userprofile%\Pictures\Windows spotlight"  
+Cd "%userprofile%\Pictures\Windows spotlight" 
+ren *.* WindowsSpotlight_???????.jpg
+
+
